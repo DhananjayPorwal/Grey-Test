6c1c13ebd625ec8072005f6dc706b6c575de6f6d
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2020-09-01 21:17:01 +0530

    Create 025 Print numbers from n to 1.c

---
+++ 025 Print numbers from n to 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int i=1, n;
+    printf("Enter : n \n");
+    scanf("%d", &n);
+    i=n;
+    do
+     {
+    printf("%d\n",i);
+    i--;
+    }
+    while (i>=1);
+
+}
