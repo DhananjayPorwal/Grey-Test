44b01406f1e0ccb27a5ee04031a1810cfe5ee2c6
DhananjayPorwal <dporwal985@gmail.com>
2020-09-06 18:34:01 +0000

    Commit

---
+++ C++ Language/001 Hello World.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // iostream is called library.
+using namespace std;
+int main(){ //Program starts executing from "main" function. 
+    cout<<"Hello World" <<endl; /*cout is used to print statement.
+endl means statement print in separate line.*/
+    return 0;
+    /* Forward slash with star is used for multi line comment
+    and double slash is used for single line comment*/
+}
