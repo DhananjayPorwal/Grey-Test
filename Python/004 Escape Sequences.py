f7c445a59e59c4a6afdd7505d5ed1a677611ae18
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2020-11-05 15:20:54 +0530

    Create 004 Escape Sequences.py

---
+++ Python/004 Escape Sequences.py
@@ -0,0 +1,16 @@
+print("C:\navy")
+# This statement will not going to print C:\User\naman
+'''
+It will print
+C:\n
+avy '''
+# To solve this issue we have to use \\ in the place of \
+print("C:\\navy")
+'''
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed'''
