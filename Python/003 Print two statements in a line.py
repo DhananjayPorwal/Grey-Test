494f8dd8f30afaf6f20820740cef2197da2b7b00
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2020-11-05 15:08:23 +0530

    Create 003 Print two statements in a line.py

---
+++ Python/003 Print two statements in a line.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Hello Reader")
+# If we run this program both the print statements appears in new line.
+# To merge view both the statements in same line we use end=' '
+print("Hello World", end=' ')
+print("Hello Reader")
+# We can do this without using end=' '
+print("Hello World", "Hello Reader")
+# We can use anything in the place of space( ).
+print("Hello World", end=' Ram ')
+print("Hello Reader")
