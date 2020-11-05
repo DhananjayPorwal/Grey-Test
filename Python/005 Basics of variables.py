f8efd2721325072ad1e974c7a1f4e697046e86e0
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2020-11-05 15:44:14 +0530

    Create 005 Basics of variables.py

---
+++ Python/005 Basics of variables.py
@@ -0,0 +1,30 @@
+# This is string variable
+stringVar1 = "Hello"
+stringVar2 = "World"
+# This is integer variable
+integerVar = 33
+# This is float variable
+floatVar = 8.9
+
+print(integerVar + floatVar)
+# This will give you result 41.9
+
+# print(stringVar1 + integerVar)
+# This will give you an error
+
+print(stringVar1 + stringVar2)
+# This will give you result HelloWorld
+
+# To check the DataType of any variable
+print(type(stringVar1))
+print(type(stringVar2))
+print(type(integerVar))
+print(type(floatVar))
+
+# Typecasting of variables
+typecastingVar1="22"
+typecastingVar2="33"
+print(int(typecastingVar1) + int(typecastingVar2))
+
+# To print same statement  multiple times
+print(10 * "Hello World\n") # This will print Hello World 10 times.
